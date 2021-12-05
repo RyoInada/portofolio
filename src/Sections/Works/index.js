d5a40899166c4feb77bc4344ccfb959c31fdc584
RyoInada <ryo.inada.sapato@gmail.com>
2021-12-05 22:36:09 +0900

    不要な部分を削除

--- src/Sections/Works/index.js
+++ src/Sections/Works/index.js
@@ -1,7 +1,6 @@
-import React, { lazy } from "react";
+import React from "react";
 import styled from "styled-components";
 
-import Slider from "react-slick";
 import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
 import TextBlock from "../../components/TextBlock";
@@ -42,38 +41,6 @@ const Title = styled.h1`
 	}
 `;
 
-const Carousal = styled.div`
-	width: 50vw;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	@media only Screen and (max-width: 40em) {
-		width: 90vw;
-		.slick-slider .slick-arrow {
-			display: none;
-		}
-	}
-	.slick-slider .slick-arrow:before {
-		color: #0a0b10;
-		font-size: 1.5rem;
-		@media only Screen and (max-width: 40em) {
-			display: none;
-		}
-	}
-	.slick-slider .slick-dots button:before {
-		color: #0a0b10;
-		font-size: 1.5rem;
-	}
-	.slick-slide.slick-active {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		margin: 0;
-		padding: 0;
-		margin-bottom: 3rem;
-	}
-`;
 
 const Works = () => {
 	// const settings = {
--- src/components/SiteBlock/index.js
+++ src/components/SiteBlock/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import styled from "styled-components";
 import styled, { keyframes } from "styled-components";
 
 const move = keyframes`
@@ -46,39 +45,7 @@ const Cb = styled.div`
   }
   /* z-index: 1; */
 `;
-const Topic = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
 
-  background-color: var(--nav);
-  color: var(--white);
-  font-weight: 700;
-  font-size: calc(0.4rem + 0.4vw);
-  padding: 0.5rem 1rem;
-  border-radius: 20px;
-
-  @media only Screen and (max-width: 48em) {
-    font-size: calc(0.4rem + 0.8vw);
-  }
-`;
-const Circle = styled.span`
-  display: inline-block;
-  width: 1rem;
-  height: 1rem;
-  border-radius: 50%;
-  background-color: var(--purple);
-  margin-right: 0.5rem;
-`;
-
-const Title = styled.div`
-  font-size: calc(1rem + 1vw);
-  line-height: 1.2;
-  padding: 0.5rem 0;
-  @media only Screen and (max-width: 48em) {
-    font-size: calc(1rem + 1.5vw);
-  }
-`;
 
 const SiteInfo = styled.div`
   max-width:50%;
@@ -105,14 +72,6 @@ const Image = styled.div`
     width: 100%;
     height: 40vw;
     margin-left: 2vw;
-  }
-`;
-
-const SubText = styled.div`
-  font-size: calc(0.5rem + 0.5vw);
-  color: var(--nav2);
-  @media only Screen and (max-width: 48em) {
-    font-size: calc(0.5rem + 1vw);
   }
 `;
 
